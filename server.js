3857bbc91232589620441774a755c13e50f5fd0c
Omar <omarbaldidk@gmail.com>
2020-10-15 18:52:45 +0200

    Applied changes for heroku deployment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 require('./config/authentication');
 const express = require('express');
 const { connectToDatabase } = require('./config/database');
@@ -20,6 +21,14 @@ app.use(passport.session());
 
 //Routes
 app.use('/api', mainAPIRoute);
+
+//Production deployment
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/dist'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
